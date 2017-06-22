11810e0f6d390a3b3b6d4c2f6f0e9bdf6507a8ab
Arthur Brown <arthur.brown@email.wsu.edu>
2017-06-22 09:46:11 -0700

    Started Physics Coding

---
+++ physicsentity.py
@@ -0,0 +1,22 @@
+import coord
+class PhysicsEntity:
+	def __init__(self, velocity, position, acceleration, angular_offset, angular_speed):
+		self.velocity, self.position, self.acceleration = velocity, position, acceleration, 
+	def update(self):
+		if self.is_enabled:
+			self.position += self.velocity
+			self.velocity += self.acceleration
+	def enable(self):
+		self.is_enabled = True
+	def disable(self):
+		self.is_enabled = False
+	def next(self):
+		
+		
+# use self.vertices to calculate collision
+class CollisionMethod:
+	def __init__(self):
+		pass
+	def check_collision(self, object1, object2):
+		# return point of impact (if any) and vector normal to collision
+		pass
